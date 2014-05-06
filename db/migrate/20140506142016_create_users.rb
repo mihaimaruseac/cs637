9b280a101877d035c3db2b0185cdd5a8b49e6514
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-05-06 08:21:17 -0600

    Make active field of user be true by default.

--- db/migrate/20140506142016_create_users.rb
+++ db/migrate/20140506142016_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :password_digest
-      t.boolean :active
+      t.boolean :active, :default => true
 
       t.timestamps
     end
