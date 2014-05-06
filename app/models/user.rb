920f2ca212da5c75a47fc1f42e7c35c7510d7fa1
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-05-06 08:25:43 -0600

    Add password fileds and validate User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible :active, :name, :password_digest
+  attr_accessible :active, :name, :password, :password_confirmation
+  validates :name, presence: true
+
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates_confirmation_of :password
+  validates :password, length: { minimum: 6 }
+  has_secure_password
 end
