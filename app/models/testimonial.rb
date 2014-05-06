84a4fa82a293a663a88703d63283961ce9cca130
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-05-06 16:36:18 -0600

    Validate testimonials model.

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,3 +1,6 @@
 class Testimonial < ActiveRecord::Base
   attr_accessible :author, :text, :year
+  validates :author, presence: true
+  validates :text, presence: true
+  validates :year, presence: true
 end
