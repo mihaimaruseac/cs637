3014c73b464fa8c5a8bd8daef89b95a345a54c1b
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-05-06 11:39:32 -0600

    Add default to active flag of role

--- db/migrate/20140506173816_create_roles.rb
+++ db/migrate/20140506173816_create_roles.rb
@@ -2,7 +2,7 @@ class CreateRoles < ActiveRecord::Migration
   def change
     create_table :roles do |t|
       t.string :role
-      t.boolean :active
+      t.boolean :active, :default => true
 
       t.timestamps
     end
