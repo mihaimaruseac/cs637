9e715d2371dd9a73471a7c728bd6e6485c7e1bd1
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-05-06 20:01:52 -0600

    Validate and link Address to Profile.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ActiveRecord::Base
-  attr_accessible :city, :profile_id, :state, :street, :zip
+  attr_accessible :city, :profile, :state, :street, :zip
+
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :street, presence: true
+  validates :zip, presence: true
+  validates :profile, presence: true
+
+  belongs_to :profile, inverse_of: :address
 end
