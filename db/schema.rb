d57d0b480d9700eaff5b3c950da39465ca85b52c
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-05-06 18:46:24 -0600

    Profile belongs to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140507004514) do
+ActiveRecord::Schema.define(:version => 20140507004605) do
 
   create_table "profiles", :force => true do |t|
     t.string   "firstName"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20140507004514) do
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "roles", :force => true do |t|
