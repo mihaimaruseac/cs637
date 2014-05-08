c3b04b01545479312a991ccd8cdb40e3127d2c19
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-05-08 08:19:25 -0600

    Register Enrolment to Parent

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -4,4 +4,5 @@ class Parent < ActiveRecord::Base
   validates :profile, presence: true
   belongs_to :profile, inverse_of: :parent
   has_and_belongs_to_many :child
+  belongs_to :enrolment, inverse_of: :parent
 end
