9969710070a5096a252a81fc9321aba29017b441
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-05-08 08:17:05 -0600

    Validate Enrolment

--- app/models/enrolment.rb
+++ app/models/enrolment.rb
@@ -1,3 +1,13 @@
 class Enrolment < ActiveRecord::Base
-  attr_accessible :child_id, :course_id, :date, :parent_id, :price
+  attr_accessible :child, :course, :date, :parent, :price
+
+  validates :child, presence: true
+  validates :course, presence: true
+  validates :parent, presence: true
+  validates :date, presence: true
+  validates :price, presence: true
+
+  belongs_to :child
+  belongs_to :course
+  has_one :parent
 end
