63d8b0994942ea53382a872c88c162c2b60dd370
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-05-08 08:17:45 -0600

    Register Enrolment to Child

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,4 +4,6 @@ class Child < ActiveRecord::Base
   validates :profile, presence: true
   belongs_to :profile, inverse_of: :child
   has_and_belongs_to_many :parent
+  has_many :enrolments
+  has_many :courses, through: :enrolments
 end
